4d0d9526eebcdac65931110884558548852b7ee9
Matthew Brown <mpbhome1@gmail.com>
2018-07-18 10:59:29 -0600

    Refactoring code to make it easier to work with.

--- roygbiv/web/js/csv_file_loading.js
+++ roygbiv/web/js/csv_file_loading.js
@@ -30,7 +30,6 @@ function updateRegionDisplay(className, brain) {
         else {
             brain.hideMeshByName(name);
         }
-        
     });
 }
 
@@ -41,12 +40,10 @@ function updateRegionDisplay(className, brain) {
  * @param {*Bool} boolChecked determines if the box is to be checked or unchecked. True=Check it, False=Uncheck 
  */
 function checkBoxesByClass(className, boolChecked) {
-    
     $('.'+className).each(function() {
         $(this).prop('checked', boolChecked);
         $(this).change();
     });
-
 }
 
 /** 
@@ -84,14 +81,6 @@ function registerEvents(scope, cameraManager) {
     // Register the checkboxes that hide/show the brain.
     registerRegionCheckboxes(scope.brain);
 
-}
-
-/**
- * @function initializeDefaultValues when a new brain is rendered, we set the initial values here.
- */
-function initializeDefaultValues() {
-    // When the brain is first rendered, we want all region select check boxes to be selected.
-    checkBoxesByClass("region-select", true);
 }
 
 /**
@@ -191,11 +180,96 @@ function generateNewBrain(scope, colorsDict) {
     updateRegionDisplay("region-select", $scope.brain);
 }
 
-
 function colorChange(event) {
     $('#nav-brain').css("background-color",  event.target.value);
 }
 
+function getGeneLocationArray(csvObject, scope) {
+    try {
+        return getGeneLocDict(csvObject);
+    }
+    catch (error_message) {
+        scope.geneStatus = error_message;
+        scope.$apply();
+        return;
+    }
+}
+
+function getCSVPath(scope) {
+
+    var path = $('#upload-path').val();
+    if(path == "") {
+        $scope.fileStatus = "Empty file path submitted. Please enter a valid file path."
+        $scope.$apply();
+        return "ERROR: Invalid path!";
+    }
+    return path;
+}
+
+function deployLegend(divID, colorScale) {
+    $('#' + divID).empty()
+    colorlegend("#" + divID, colorScale, "linear", {title: "Gene Expression"});
+}
+
+function registerLoadCSVFileEvent(scope) {
+    // Upload is for uploading data on how genes are expressed on different regions of the brain.
+    $('#search-form').on('click', '#upload', function(e) { 
+        path = getCSVPath(scope);
+        d3.csv(path, function(error, csvObject) {
+            
+            // Get the file path of the users defined file, so we can display it for the user.
+            scope.filePath = path;
+            
+            if(error) {
+                // Inform the user that the file failed to load.
+                scope.fileStatus = "ERROR failed to upload file!";
+                scope.$apply();
+                
+            } else {
+
+                // Inform the user that the file successfully loaded, and display the path.
+                scope.fileStatus = "File uploaded successfully!";
+                scope.csvObject = csvObject;
+                scope.$apply();
+            }
+        });
+    });
+}
+
+function registerRenderBrainEvent(scope) {
+    $('#search-form').on('click', '#search-button', function(e) {
+        e.preventDefault();
+
+        var geneName = scope.geneSearch.trim().toUpperCase()
+
+        geneLocationArray = getGeneLocationArray(scope.csvObject, scope); 
+
+        var rawData = cleanData(scope.csvObject, geneLocationArray);
+
+        scope.regionColorFactory = new RegionColorFactory(
+            rawData, getHeader(), scope.colorPickerR,
+            scope.colorPickerG, scope.colorPickerB
+        );
+
+        if(scope.zScoreCheckbox) {
+            scope.regionColorFactory.setNormalizationState("zScoreRow");
+        }
+        
+        try {
+            scope.regionColorFactory.setColors(scope.colorPickerR, scope.colorPickerG, scope.colorPickerB);
+        } catch (error) {
+            scope.minMidMaxStatus = error;
+            scope.$apply();
+        }
+        
+        regionColorArray = scope.regionColorFactory.generateRegionColorArray(geneName);
+        scope.brain = generateNewBrain(scope, regionColorArray);
+
+        deployLegend("nav_legend", colorScale);
+        checkBoxesByClass("region-select", true);
+        registerEvents(scope, scope.cameraManager);
+    });
+}
 
 // Set up the module/controller for Uploading the Brain CSV file, and displaying the brain based off of the
 // Main entry point for the application
@@ -204,101 +278,10 @@ angular.module('navigator', []).controller('NavigateController', ['$scope', func
 
     setupDefaultValues($scope);
 
-    // Load CSV file
+    // Load CSV file containing gene expression data for the brain regions.
+    registerLoadCSVFileEvent($scope);
 
-    // Select Gene
+    // Events associated with rendering the brain and selection options.
+    registerRenderBrainEvent($scope);
 
-    // Clean the data
-
-    // get the color data
-
-    // select options
-
-    // build the brain
-
-    // register events
-    
-    // Upload is for uploading data on how genes are expressed on different regions of the brain.
-    $('#search-form').on('click', '#upload', function(e) { 
-        var path = $('#upload-path').val();
-        if(path == "") {
-            $scope.fileStatus = "Empty file path submitted. Please enter a valid file path."
-            $scope.$apply();
-            return;
-        }
-        d3.csv(path, function(error, csvObject) {
-            
-            // Get the file path of the users defined file, so we can display it for the user.
-            $scope.filePath = path;
-            
-            if(error) {
-                // Inform the user that the file failed to load.
-                $scope.fileStatus = "ERROR failed to upload file!";
-                $scope.$apply();
-                
-            } else {
-
-                // Inform the user that the file successfully loaded, and display the path.
-                $scope.fileStatus = "File uploaded successfully!";
-                $scope.$apply();
-
-                //attempt at making an input bar for genes
-                // Load a new brain when the user clicks Go
-                $('#search-form').on('click', '#search-button', function(e) {
-                    e.preventDefault();
-
-                    // Currently, values in 'keyObj' and indexes in 'datas' are identical
-                    var geneName = $scope.geneSearch.trim().toUpperCase()
-                    var geneLocDictionary;
-
-                    // Attempt to get the Gene from the User. Throw a message if it fails.
-                    try {
-                        geneLocDictionary = getGeneLocDict(csvObject);
-                    }
-                    catch (error_message) {
-                        $scope.geneStatus = error_message;
-                        $scope.$apply();
-                        return;
-                    }
-
-                    // Get the brain data related to the specific Gene Location.
-                    // var geneRegionDataObject = getRegionDict(geneLoc, csvObject);
-                    // var geneRegionDataArray = Object.values(geneRegionDataObject);
-                    // geneRegionDataArray = geneRegionDataArray.map(Number);
-
-                    var rawData = cleanData(csvObject, geneLocDictionary);
-
-                    // Settup our gene data expression factory.
-                    $scope.regionColorFactory = new RegionColorFactory(
-                        rawData, getHeader(), $scope.colorPickerR,
-                        $scope.colorPickerG, $scope.colorPickerB
-                    );
-
-                    // Get the color scale and colored data for the Brain.
-                    // If the zscore box is checked, use zscore for rendering the brain. Otherwise use min/max.
-                    // TODO: Modify code so upon error, it goes not to minMidMaxStatus but something
-                    // more general.
-                    if($scope.zScoreCheckbox) {
-                        $scope.regionColorFactory.setNormalizationState("zScoreRow");
-                    }
-                    
-                    try {
-                        $scope.regionColorFactory.setColors($scope.colorPickerR, $scope.colorPickerG, $scope.colorPickerB);
-                    } catch (error) {
-                        $scope.minMidMaxStatus = error;
-                        $scope.$apply();
-                    }
-                    
-                    dict = $scope.regionColorFactory.generateRegionColorArray(geneName);
-                    //render new brain
-                    $scope.brain = generateNewBrain($scope, dict);
-                 
-                    $('#nav_legend').empty()
-                    colorlegend("#nav_legend", colorScale, "linear", {title: "Gene Expression"});
-                    initializeDefaultValues();  
-                    registerEvents($scope, $scope.cameraManager);
-                });
-            }
-        });
-    });
 }]);
